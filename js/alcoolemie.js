99fac63d7b0555345ab1cafc1b18bf08e9f2d46b
DUSSART Luke <lukedussart@hotmail.fr>
2020-12-07 11:07:51 +0100

    pratiquement fini test affichage

--- js/alcoolemie.js
+++ js/alcoolemie.js
@@ -1,4 +1,27 @@
 
+window.addEventListener('load', function () {
+    // tabEvents est une collection d'évenements
+    let tabEvents = ['keyup', 'click'];
+
+    // tabInputs est une collection de <input>
+    let tabInputs = window.document.querySelectorAll('input[type="number"]');
+
+    // Parcours de tabInputs en s'appuyant sur le nombre de <input> et sur tabEvents
+    for (let i = 0; i < tabInputs.length; i++) {
+        for (let j = 0; j < tabEvents.length; j++) {
+            // Ajout d'un Listener sur tous les <input> sur les évènements listés dans tabEvents
+            tabInputs[i].addEventListener(tabEvents[j], gestionAlcoolemie);
+        }
+    }
+
+    // Gestion de l'input de type range (recopie de la valeur dans l'output)
+    window.document.querySelector('#sexe input[type="radio"]:checked').addEventListener('change', function () {
+        window.document.querySelector('#sexe input[type="radio"]:checked').value = getString('#sexe input[type="radio"]:checked');
+        gestionAlcoolemie();
+    });
+
+});
+
 
 /**
  * Fonction qui retourne l'alcool pur ingéré en fonction du nombre
@@ -21,7 +44,7 @@ function getAlcoolPur(nbVerres) {
  */
 function getCoefDiffusion(sexe) {
   const coefDiffuH = 0.7, coefDiffuF = 0.6;
-  if (sexe === 'M') {
+  if (sexe === 'homme') {
     return coefDiffuH;
   } else {
     return coefDiffuF;
@@ -55,7 +78,10 @@ function getAlcoolemie(sexe, poids, nbVerres) {
  */
 function getAmende(alcoolemie) {
   const seuil = 0.8;
-  if (alcoolemie < seuil) {
+  const seuilMin = 0.5;
+  if(alcoolemie <seuilMin){
+    return 'Pas d\'amende';
+  } else if ( alcoolemie < seuil) {
     return 'Minoree : 90 euros / Forfaitaire : 135 euros / Majoree : 375 euros';
   } else {
     return '4500 euros';
@@ -70,10 +96,71 @@ function getAmende(alcoolemie) {
  * @returns {String}
  */
 function getSanction(alcoolemie) {
-  const seuil = 0.8;
-  if (alcoolemie < seuil) {
+  const seuil = 0.8;const seuilMin = 0.5;
+  if(alcoolemie <seuilMin){
+    return 'Pas de sanction';
+  } else if ( alcoolemie < seuil) {
     return '6 points + suspension 3 ans';
   } else {
     return '6 points + 2 ans de prison + suspension 3 ans + stage de sensibilisation';
   }
+}
+
+
+/**
+ * Fonction qui retourne une valeur entière récupérée via
+ * window.document.querySelector(id)
+ *
+ * @param {string} id
+ * @returns {integer}
+ */
+function getInt(id) {
+  let valeur = parseInt(getString(id));
+  if (isNaN(valeur)) {
+    window.document.querySelector(id).value = 0;
+    return 0;
+  } else {
+    return valeur;
+  }
+}
+
+/**
+ * Fonction qui retourne un string récupéré dans un champ via son id
+ *
+ * @param {string} id
+ * @returns {string}
+ */
+function getString(id) {
+  return window.document.querySelector(id).value;
+}
+
+function gestionAlcoolemie() {
+  const sexe = getString('#sexe input[type="radio"]:checked');
+  const poids = getInt('#num_poids');
+  const verres = getInt('#num_verre');
+  const alcoolemie = getAlcoolemie(sexe, poids, verres);
+  let alH3 = window.document.querySelector('#alcoolemie');
+  let amH3 = window.document.querySelector('#amende');
+  let saH3 = window.document.querySelector('#sanction');
+  /* utilisation de #remuneration au lieu de #prime pour réutiliser les règles
+   * CSS définie dans simulateur.css
+   * Si #remuneration (<h3 id='alcoolemie'></h3>) n'existe pas, on le créé */
+  if (!alH3) {
+    alH3 = window.document.createElement('h3');
+    alH3.id = 'alcoolemie';
+    window.document.querySelector('#recueilinfos').appendChild(alH3);
+  }
+  if (!amH3) {
+    amH3 = window.document.createElement('h3');
+    amH3.id = 'amende';
+    window.document.querySelector('#recueilinfos').appendChild(amH3);
+  }
+  if (!saH3) {
+    saH3 = window.document.createElement('h3');
+    saH3.id = 'sanction';
+    window.document.querySelector('#recueilinfos').appendChild(saH3);
+  }
+  alH3.innerHTML = alcoolemie + 'g/l de sang';
+  amH3.innerHTML = getAmende(alcoolemie);
+  saH3.innerHTML = getSanction(alcoolemie);
 }
--- test/unit/testAlcoolemie.js
+++ test/unit/testAlcoolemie.js
@@ -1,33 +1,86 @@
 /**
  * Tests unitaires du projet Alcoolémie
- * 
+ * pour la partie back 
  * 
  */
 MesTestsUnitaires = TestCase('AlcoolemieTest');
-MesTestsUnitaires.prototype.testGetAlcoolPur = function(){
+MesTestsUnitaires.prototype.testGetAlcoolPur = function () {
   assertEquals('0 verre', 0, getAlcoolPur(0));
   assertEquals('1 verre', 10, getAlcoolPur(1));
 }
 
-MesTestsUnitaires.prototype.testGetCoefDiffusion = function(){
+MesTestsUnitaires.prototype.testGetCoefDiffusion = function () {
   assertEquals('Homme', 0.7, getCoefDiffusion('M'));
   assertEquals('Femme', 0.6, getCoefDiffusion('F'));
 }
 
-MesTestsUnitaires.prototype.testsGetAlcoolemie = function() {
- assertEquals('Homme 100 kg 1 verre', 0.14, getAlcoolemie('M', 100, 1));
- assertEquals('Femme 100 kg 1 verre', 0.17, getAlcoolemie('F', 100, 1));
+MesTestsUnitaires.prototype.testsGetAlcoolemie = function () {
+  assertEquals('Homme 100 kg 1 verre', 0.14, getAlcoolemie('M', 100, 1));
+  assertEquals('Femme 100 kg 1 verre', 0.17, getAlcoolemie('F', 100, 1));
 };
 
-MesTestsUnitaires.prototype.testsGetAmende = function() {
- assertEquals('Moins de 0,8 g/l de sang', 'Minoree : 90 euros / Forfaitaire : 135 euros / '+
-'Majoree : 375 euros', getAmende(0.4));
- assertEquals('A partir de 0,8 g/l', '4500 euros', getAmende(0.8));
+MesTestsUnitaires.prototype.testsGetAmende = function () {
+  assertEquals('Moins de 0,8 g/l de sang', 'Minoree : 90 euros / Forfaitaire : 135 euros / ' +
+          'Majoree : 375 euros', getAmende(0.4));
+  assertEquals('A partir de 0,8 g/l', '4500 euros', getAmende(0.8));
 };
 
-MesTestsUnitaires.prototype.testsGetSanction = function() {
- assertEquals('Moins de 0,8 g/l de sang', '6 points + suspension 3 ans',
-getSanction(0.4));
- assertEquals('A partir de 0,8 g/l de sang', '6 points + 2 ans de prison + suspension ' +
-'3 ans + stage de sensibilisation', getSanction(0.8));
+MesTestsUnitaires.prototype.testsGetSanction = function () {
+  assertEquals('Moins de 0,8 g/l de sang', '6 points + suspension 3 ans',
+          getSanction(0.4));
+  assertEquals('A partir de 0,8 g/l de sang', '6 points + 2 ans de prison + suspension ' +
+          '3 ans + stage de sensibilisation', getSanction(0.8));
+};
+
+/**
+ * Tests unitaires du projet Alcoolémie
+ * pour la partie graphique
+ * 
+ */
+MesTestsUnitaires.prototype.testsGetInt = function () {
+  /*:DOC +=
+   <input type="number" id="num_verre" value="1">
+   <input type="number" id="num_poids" value="100">
+   */
+  assertTrue('Test poids 100 Kg', 100 === getInt('#num_poids'));
+  assertTrue('Test 1 verre', 1 === getInt('#num_verre'));
+  window.document.querySelector('#num_verre').value = 'texte';
+  assertTrue('Test erreur saisie verre', 0 === getInt('#num_verre'));
+};
+
+
+MesTestsUnitaires.prototype.testsGetString = function () {
+  /*:DOC +=
+   <fieldset id="sexe">
+   <input type="radio" name="rd_sexe" id="rd_sexehomme" value="homme"
+   checked="checked">
+   <input type="radio" name="rd_sexe" id="rd_sexefemme" value="femme">
+   </fieldset>
+   */
+  assertTrue('Test bouton radio Homme',
+          'homme' === getString('#sexe input[type="radio"]:checked'));
+  // Changement de sexe
+  window.document.querySelector('#rd_sexehomme').removeAttribute('checked');
+  window.document.querySelector('#rd_sexefemme').setAttribute('checked', 'checked');
+  assertTrue('Test bouton radio Femme',
+          'femme' === getString('#sexe input[type="radio"]:checked'));
+};
+
+
+MesTestsUnitaires.prototype.testsGestionAlcoolemie = function () {
+  /*:DOC +=
+   <input type="number" id="num_verre" value="1">
+   <input type="number" id="num_poids" value="100">
+  <fieldset id="sexe">
+   <input type="radio" name="rd_sexe" id="rd_sexehomme" value="homme"
+   checked="checked">
+   <input type="radio" name="rd_sexe" id="rd_sexefemme" value="femme">
+   </fieldset>
+  <h3 id="alcoolemie" value="0.57 g/l de sang"></h3>
+  <h3 id="amende" value="Minoree : 90 euros / Forfaitaire : 135 euros / Majoree : 375 euros"></h3>
+  <h3 id="sanction" value ="6 points + suspension 3 ans"></h3>
+   */
+  assertTrue('Test gestionAlcoolemie', '0.57g/l de sang' === get('#alcoolemie'));
+  assertTrue('Test gestionAlcoolemie', 'Minoree : 90 euros / Forfaitaire : 135 euros / Majoree : 375 euros' === getString('#amende'));
+  assertTrue('Test gestionAlcoolemie', '6 points + suspension 3 ans' === getString('#sanction'));
 };
